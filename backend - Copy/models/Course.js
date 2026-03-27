5dc4fde6ae8fb01e6b38c10e8eb73cedc472e5e5
khadeejamatloob <khadeejamatloob887@gmail.com>
2026-03-27 19:59:41 +0500

    deleted duplicate folders

--- backend - Copy/models/Course.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CourseSchema = new mongoose.Schema({
-  title: String,
-  instructor: String,
-  progress: Number,
-});
-
-module.exports = mongoose.model('Course', CourseSchema);
